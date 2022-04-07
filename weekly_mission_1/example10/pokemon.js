a07d4ee9f7c4dcf528f921128caa337276c4fe71
cdgonzalez <carlkillrs@gmail.com>
2022-04-07 10:38:06 -0500

    Se agrega el ejemplo 10 de JS en otra carpeta "example10"

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,8 @@
+export default class MyPokemon {
+    constructor (name) { //Constructor
+        this.name = name
+    }
+    sayHello() { //Funcion saludo =D
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+    }
+}
